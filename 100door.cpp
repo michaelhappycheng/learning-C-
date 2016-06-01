c0b09a3e2b002200c68393488b77ac42f1d5b2c0
Michael Cheng <cheng12@usc.edu>
2016-05-31 17:56:08 -0700

    Add files via upload

---
+++ 100door.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  100door
+//
+//  Created by Michael Cheng on 5/31/16.
+//  Copyright © 2016 Michael Cheng. All rights reserved.
+//
+
+
+/*
+ http://rosettacode.org/wiki/100_doors
+ Problem: You have 100 doors in a row that are all initially closed. You make 100 passes by the doors. The first time through, you visit every door and toggle the door (if the door is closed, you open it; if it is open, you close it). The second time you only visit every 2nd door (door #2, #4, #6, ...). The third time, every 3rd door (door #3, #6, #9, ...), etc, until you only visit the 100th door.
+ 
+ Question: What state are the doors in after the last pass? Which are open, which are closed?
+ 
+ Alternate: As noted in this page's discussion page, the only doors that remain open are whose numbers are perfect squares of integers. Opening only those doors is an optimization that may also be expressed; however, as should be obvious, this defeats the intent of comparing implementations across programming languages.
+
+*/
+ 
+ 
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
